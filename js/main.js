028bf5311df9d71ee58082404a72a625690c878f
Raul <rey_brujo@hotmail.com>
2020-11-25 21:18:07 +0900

    add work content

--- js/main.js
+++ js/main.js
@@ -66,42 +66,6 @@ $('#top-contact').click(function(){
 });
 // end switch contents
 
-/*
-
-// send mail function
-var mandrill = require('node-mandrill')('<your API Key>'); 
-
-function sendEmail ( _name, _email, _subject, _message) {
-    mandrill('/messages/send', {
-        message: {
-            to: [{email: _email , name: _name}],
-            from_email: 'noreply@raulet.dev',
-            subject: _subject,
-            text: _message
-        }
-    }, function(error, response){
-        if (error) console.log( error );
-        else console.log(response);
-    });
-}
-
-// define your own email api which points to your server.
-
-app.post( '/api/sendemail/', function(req, res){
-
-    var _name = req.body.name;
-    var _email = req.body.email;
-    var _subject = req.body.subject;
-    var _messsage = req.body.message;
-
-    //implement your spam protection or checks. 
-
-    sendEmail ( _name, _email, _subject, _message );
-
-});
-
-*/
-
 // load screen fade off
 $('#load').fadeOut(1500);
 });
